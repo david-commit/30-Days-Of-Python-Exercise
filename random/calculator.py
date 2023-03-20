b4b9563b0907f40ec51f019472357493e66a6698
david-commit <davidondiege@gmail.com>
2023-03-20 09:50:34 +0300

    Complete Calculator

--- random/calculator.py
+++ random/calculator.py
@@ -1,5 +1,27 @@
 # Build a calculator
 
-num1 = input('Enter first number: ')
-operation = input('Enter operation to perform(+,-,*,/,%): ')
-num1 = input('Enter first number: ')
+num1 = input('Enter first integer: ')
+operation = input('Enter operation to perform(+,-,*,/,%,//): ')
+num2 = input('Enter second integer: ')
+
+def calculator(num1, operation, num2):
+    int1 = int(num1)
+    int2 = int(num2)
+
+    # Check if operand is valid then operate
+    if(operation == '+'):
+        return int1 + int2
+    elif(operation == '-'):
+        return int1 - int2
+    elif(operation == '*'):
+        return int1 * int2
+    elif(operation == '/'):
+        return int1 / int2
+    elif(operation == '%'):
+        return int1 % int2
+    elif(operation == '//'):
+        return int1 // int2
+    else:
+        return 'Entered operand is not valid'
+    
+print(calculator(num1, operation, num2))
